8c83da495193822b8c2445b1c31dfc08932c44c8
imnileshh <nilesh.213779101@vcet.edu.in>
2025-02-04 19:22:18 +0530

    add cloudinary and multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_CLOUD_API,
+    api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET
+});
+
+const uploadOnCloudinary = async (fileLocalPath) => {
+    try {
+        if (!fileLocalPath) return null
+
+        const response = await cloudinary.uploader.upload(fileLocalPath, {
+            resource_type: "auto"
+        })
+        console.log(response)  // check other response.options
+        return response
+    } catch (error) {
+        // if upload fails delete from loacal server
+        fs.unlinkSync(fileLocalPath)
+        console.log("Failed to uplaod on Cloudinary:", error)
+        return null
+    }
+}
+
+export { uploadOnCloudinary }
